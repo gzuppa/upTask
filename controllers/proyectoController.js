c0e6aafb4da50e51ffe241bc113f7a870b05780c
Gustavo Zuppa <gustavo.zuppa@benandfrank.com>
2021-01-23 22:05:17 -0600

    Add new project route and view

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -3,3 +3,9 @@ exports.proyectoHome = (req, res) => {
         nombrePagina: 'Proyectos'
     })
 };
+
+exports.formularioProyecto = (req, res) => {
+    res.render('nuevoProyecto', {
+        nombrePagina: 'Nuevo Proyecto'
+    })
+}
