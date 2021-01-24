7fff28ab8184122ffc7636b4a09a5f29f9afbe9f
Gustavo Zuppa <gustavo.zuppa@benandfrank.com>
2021-01-23 22:17:11 -0600

    Send form to server

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express')
 const router = express.Router()
 const proyectoController = require('../controllers/proyectoController')
@@ -6,5 +7,6 @@ module.exports = () => {
     //hOME ROUTE
     router.get('/', proyectoController.proyectoHome);
     router.get('/nuevo-proyecto', proyectoController.formularioProyecto);
+    router.post('/nuevo-proyecto', proyectoController.nuevoProyecto);
     return router;
 }
