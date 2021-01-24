bcf792a62aa47114c61aad44a1934ab9bbcf5291
Gustavo Zuppa <gustavo.zuppa@benandfrank.com>
2021-01-23 20:43:07 -0600

    Creating controller

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const proyectoController = require('../controllers/proyectoController')
 
 module.exports = () => {
     //hOME ROUTE
-    router.get('/', (req, res) => {
-        res.send('Bienvenido')
-    });
-    router.get('/nosotros', (req, res) => {
-        res.send('Nosotros')
-    });
+    router.get('/', proyectoController.proyectoHome)
     return router;
 }
