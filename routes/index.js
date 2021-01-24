be8501c9774c0dc77551f6073a50d3ec7cd42430
Gustavo Zuppa <gustavo.zuppa@benandfrank.com>
2021-01-24 13:57:26 -0600

    Sanitization of data before a database append

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
-const express = require('express')
-const router = express.Router()
-const proyectoController = require('../controllers/proyectoController')
+const express = require('express');
+const router = express.Router();
+const proyectoController = require('../controllers/proyectoController');
+const { body } = require('express-validator/check');
 
 module.exports = () => {
     //hOME ROUTE
     router.get('/', proyectoController.proyectoHome);
-    router.get('/nuevo-proyecto', proyectoController.formularioProyecto);
-    router.post('/nuevo-proyecto', proyectoController.nuevoProyecto);
+    router.get('/nuevo-proyecto', 
+        proyectoController.formularioProyecto);
+    router.post('/nuevo-proyecto', 
+        body('nombre').not().isEmpty().trim().escape(), //not for negation, trim to delete empty spaces
+        proyectoController.nuevoProyecto);
     return router;
 }
